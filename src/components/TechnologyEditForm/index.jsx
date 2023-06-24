54cde44a1dfeea8636d95f2a2c65a124e13beb6a
svnoliveira <srkuran@gmail.com>
2023-06-23 23:12:22 -0300

    style: adicionado disabled no input nome no componente TechnologyEditForm

--- src/components/TechnologyEditForm/index.jsx
+++ src/components/TechnologyEditForm/index.jsx
@@ -24,7 +24,7 @@ export const TechnologyEditForm = () => {
             {isLoading ? <Loading /> :
                 <StyledForm onSubmit={handleSubmit(submitEdit)} noValidate>
 
-                    <Input register={register} errors={errors} label="Nome" saveTag="title" readonly
+                    <Input register={register} errors={errors} label="Nome" saveTag="title" disabled
                         type="text" id="tech-register-name" value={getCurrentTechNameById(clickedCard)} />
 
                     <SelectStatus register={register} errors={errors} />
